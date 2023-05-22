41403994fe6b5c698d4b28c7566c1179f9fa403f
franckDev21 <82069062+franckDev21@users.noreply.github.com>
2023-05-22 18:09:42 +0100

    Update Input.tsx

--- Input.tsx
+++ Input.tsx
@@ -1,77 +1,75 @@
-  import React, { ChangeEvent, forwardRef, InputHTMLAttributes } from 'react';
-  import classNames from 'classnames';
-  import { UseFormRegisterReturn } from 'react-hook-form';
+import React, { ChangeEvent, forwardRef, InputHTMLAttributes } from 'react';
+import classNames from 'classnames';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
-  export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    containerClassName?: string;  
-    inputClassName?: string;
-    label?: string;
-    isDisabled?: boolean; 
-    isInvalid?: boolean;   
-    isRequired?: boolean;
-    errorMessage?: string; 
-    handleChange?: (e:ChangeEvent<HTMLInputElement>) => any;
-    register?: UseFormRegisterReturn;
-  }
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  containerClassName?: string;  
+  inputClassName?: string;
+  label?: string;
+  isDisabled?: boolean;
+  isInvalid?: boolean;
+  isRequired?: boolean;
+  errorMessage?: string; 
+  handleChange?: (e:ChangeEvent<HTMLInputElement>) => any;
+  register?: UseFormRegisterReturn;
+}
+
+// eslint-disable-next-line react/display-name
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      containerClassName = 'flex flex-col',
+      inputClassName = 'w-full px-4 py-2 border rounded-md',
+      label,
+      isDisabled,
+      isInvalid,
+      errorMessage,
+      isRequired,
+      handleChange = () => {},
+      register,
+      ...rest
+    },
+    ref 
+  ) => {
     
-
-  const Input = forwardRef<HTMLInputElement, InputProps>(
-    (
-      {
-        containerClassName = 'flex flex-col',
-        inputClassName = 'w-full px-4 py-2 border rounded-md',
-        label,
-        isDisabled,
-        isInvalid,
-        errorMessage,
-        isRequired,
-        handleChange = () => {},
-        register,
-        ...rest
-      },
-      ref 
-    ) => {
-      
-      return (
-        <div className={containerClassName}>
-          {label && (
-            <label
-              className="mb-2 text-sm font-medium text-gray-700"
-              htmlFor={rest.id}
-            >
-              {label}
-              {isRequired && <span className="text-red-500">*</span>}
-            </label>
+    return (
+      <div className={containerClassName}>
+        {label && (
+          <label
+            className="mb-2 text-sm font-medium"
+            htmlFor={rest.id}
+          >
+            {label}
+            {isRequired && <span className="text-red-500">*</span>}
+          </label>
+        )}
+        <input
+          ref={ref}
+          {...register}
+          onChange={e => {
+            handleChange(e)
+            register?.onChange(e)
+          }}
+          className={classNames(
+            inputClassName,
+            {
+              'bg-gray-100 cursor-not-allowed': isDisabled,
+              'border-red-500': isInvalid,
+            },
+            'border-0  focus:outline-none outline-none'
           )}
-          <input
-            ref={ref}
-            {...register}
-            onChange={e => {
-              handleChange(e)
-              register?.onChange(e)
-            }}
-            className={classNames(
-              inputClassName,
-              {
-                'bg-gray-100 cursor-not-allowed': isDisabled,
-                'border-red-500': isInvalid,
-              },
-              'border-0  focus:outline-none outline-none'
-            )}
-            disabled={isDisabled}
-            aria-invalid={isInvalid}
-            required={isRequired}
-            {...rest}
-          />
-          {isInvalid && (
-            <p className="mt-2 text-sm font-medium text-red-500">{errorMessage}</p>
-          )}
-        </div>
-      );
-    }
-  );
+          disabled={isDisabled}
+          aria-invalid={isInvalid}
+          required={isRequired}
+          {...rest}
+        />
+        {isInvalid && (
+          <p className="mt-2 text-sm font-medium text-red-500">{errorMessage}</p>
+        )}
+      </div>
+    );
+  }
+);
 
-  export default Input;
+export default Input;
 
-          
-          
